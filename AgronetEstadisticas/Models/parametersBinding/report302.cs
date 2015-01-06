68bfcd4cf5238ebd19ee61a00395f46b83fff3fd
David Alméciga <wdavid@dagrinchi.com>
2015-01-06 15:45:03 -0500

    101, 302 seaApi

---
+++ AgronetEstadisticas/Models/parametersBinding/report302.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AgronetEstadisticas.Models.parametersBinding
+{
+    public class report302 : report
+    {
+        public DateTime fecha_inicial { get; set; }
+        public DateTime fecha_final { get; set; }
+    }
+}
